c42b1ad0cdadeeee416c29cd90ea9bf25783f809
Gpanayu <gpanayu@yahoo.co.th>
2020-07-19 14:49:31 +0700

    add MCC script and typical models Keras

---
+++ tools/MCC.py
@@ -0,0 +1,19 @@
+import keras.backend as K
+
+def matthews_correlation(y_true, y_pred):
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    y_pred_neg = 1 - y_pred_pos
+
+    y_pos = K.round(K.clip(y_true, 0, 1))
+    y_neg = 1 - y_pos
+
+    tp = K.sum(y_pos * y_pred_pos)
+    tn = K.sum(y_neg * y_pred_neg)
+
+    fp = K.sum(y_neg * y_pred_pos)
+    fn = K.sum(y_pos * y_pred_neg)
+
+    numerator = (tp * tn - fp * fn)
+    denominator = K.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
+
+    return numerator / (denominator + K.epsilon())
---
+++ typicalModels/4Conv.py
@@ -0,0 +1,24 @@
+# Import Keras and other essential libs
+
+def CNN4Conv(window):
+        
+        # create model
+        inp = Input((1, window, 12))
+        x = Conv2D(32, (5, 5), input_shape=(1, window, 12), activation='relu')(inp)
+        x = Conv2D(32, (3, 3), activation='relu')(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Conv2D(64, (2, 2), activation='relu')(x)
+        x = Conv2D(64, (2, 2), activation='relu')(x)
+        x = Dropout(0.5)(x)
+        x = Flatten()(x)
+        out1 = Dense(128, activation='relu')(x)
+        out1 = Dropout(0.5)(out1)
+        out1 = Dense(2, activation='softmax')(out1)
+        out2 = Dense(128, activation='relu')(x)
+        out2 = Dropout(0.5)(out2)
+        out2 = Dense(2, activation='softmax')(out2)
+        model = Model(inp, [out1, out2])
+        
+        # compile model
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', matthews_correlation])
+        return model
